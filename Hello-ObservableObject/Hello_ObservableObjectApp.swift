42bda20708fea04832220d6e1820a85bd929bbe7
NazarStf <hworker98@gmail.com>
2023-06-02 16:20:40 +0300

    Initial Commit

---
+++ Hello-ObservableObject/Hello_ObservableObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_ObservableObjectApp.swift
+//  Hello-ObservableObject
+//
+//  Created by NazarStf on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hello_ObservableObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
